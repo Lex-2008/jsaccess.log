0e42fdd6d49bb02bf814189a8fdc30fb262de82e
Alexey Shpakovsky <alexey@shpakovsky.ru>
2015-01-10 05:52:33 +0100

    'wget --no-verbose' for less verbosity when downloading geoip data

--- geoip.sh
+++ geoip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cut -d ' ' -f 1 $(dirname $0)/previous.log | sort -u | wget -i - -B http://freegeoip.net/csv/ -O $(dirname $0)/geoip.txt
+cut -d ' ' -f 1 $(dirname $0)/previous.log | sort -u | wget -nv -i - -B http://freegeoip.net/csv/ -O $(dirname $0)/geoip.txt
